5b7cd3637d0ccf99eb762d88875e6ffa450af350
Luan Severo <luansevero@icloud.com>
2022-07-28 12:18:50 -0300

    Bonus Answer

--- .sql
+++ .sql
@@ -24,3 +24,15 @@ ON j."roleId" = r.id
 WHERE j.active = true
 GROUP BY r.name
 ORDER BY MAX(salary) ASC;
+
+--Questão Bônus
+SELECT s.name AS school,c.name AS course, COUNT("userId") AS "studentsCount", e.status AS role
+FROM educations e
+JOIN schools s
+ON e."schoolId" = s.id
+JOIN courses c
+ON e."courseId" = c.id
+WHERE e.status = 'ongoing' OR e.status = 'finished'
+GROUP BY s.name, c.name, e.status
+ORDER BY COUNT("userId") DESC
+LIMIT 3;
