3a6df7cfa9f6e4012371b502558d2154580c9bf9
Luan Severo <luansevero@icloud.com>
2022-07-28 12:08:05 -0300

    4th Answer

--- .sql
+++ .sql
@@ -15,3 +15,12 @@ JOIN users u
 ON "writerId" = u.id
 WHERE "writerId" = 435
 GROUP BY u.name, "writerId";
+
+--Questão 4
+SELECT MAX(salary) AS "maximumSalary", r.name AS role
+FROM jobs j
+JOIN roles r
+ON j."roleId" = r.id
+WHERE j.active = true
+GROUP BY r.name
+ORDER BY MAX(salary) ASC;
